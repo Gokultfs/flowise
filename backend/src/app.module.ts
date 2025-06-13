8b2910e35d275e22f7e629cbba4845b2613beaf9
Gokultfs <gokuls1@ltfs.com>
2025-06-13 17:58:41 +0530

    Initial commit: Backend NestJS app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+`GOKUL THERE Is No Comment` Let's Save it
+\@module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
