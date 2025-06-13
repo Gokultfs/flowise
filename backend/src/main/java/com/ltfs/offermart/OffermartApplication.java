5bd96cbf6dcc3ea6ce05d06884227db65d5771e8
Gokultfs <gokuls1@ltfs.com>
2025-06-13 11:57:48 +0530

    feat: Create main Spring Boot application file

---
+++ backend/src/main/java/com/ltfs/offermart/OffermartApplication.java
@@ -0,0 +1,13 @@
+package com.ltfs.offermart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@PringBootApplication
+public class OffermartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffermartApplication.class, args);
+    }
+
+}
