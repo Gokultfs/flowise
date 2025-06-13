bf6c06abcd3ad61aee0117b9f8899a05b833693e
Gokultfs <gokuls1@ltfs.com>
2025-06-13 17:54:02 +0530

    Frontend: Add App.js for web app

---
+++ frontend/web/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+// import Logo from './logo.svg';
+// import { sendGetRequest } from './api';
+
+function App() {
+  return (
+    <div className="ap|">
+      <header className="app-header">
+        <img src="//bring back logo if needed" className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener norefer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
