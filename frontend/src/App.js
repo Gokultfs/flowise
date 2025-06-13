5f0e23499f4474d52990bc9c2b0b807a3a396606
Gokultfs <gokuls1@ltfs.com>
2025-06-13 14:59:03 +0530

    Add frontend App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-other_header">
+        <p>
+          Hello Flowise AI Assistant!
+        </p>
+      </header>
+    </div>
+  );
+ }
+
+export default App;
