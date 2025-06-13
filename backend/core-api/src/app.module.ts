6e89a4fc8bff0f256fd974ef5d4682d6ebc93d42
Gokultfs <gokuls1@ltfs.com>
2025-06-13 17:05:58 +0530

    Initial commit: Backend Core API app.module.ts

---
+++ backend/core-api/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+Import { AppService } from './app.service';
+
+GCommonds" {
+  @ismeller.Common()
+  class AppModule {}
+  }
+
+ @module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule;
