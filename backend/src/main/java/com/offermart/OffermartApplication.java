f023529bd68db69f4551bdbc836c1a03fe5d42eb
Gokultfs <gokuls1@ltfs.com>
2025-06-13 14:10:06 +0530

    Delete backend directory

--- backend/src/main/java/com/offermart/OffermartApplication.java
+++
@@ -1,14 +0,0 @@
-package com.offermart;
-import org.springgëjg°¢¹˜›Ûİ”Üš[™Ğ›Ûİ\XØ][ÛÂš[\ÜÜ™ËœÜš[™ÙúÚ™ì(®Bæ&ö÷BæWFö6öæf–wW&Rå7&–æt&ö÷D6öæf–wW&F–öã°¦–×÷'B÷&rç7&–ævg&ÖWv÷&²æWFö6öæf–wW&RäVæ&ÆT6ö×öæVçE66ã°¦–×÷'B÷&rç7&–æv~¶¦{
-+¹…¹¹½Ñ…Ñ¥½¸¹‘¹…‰±•]•‰Í•ÕÉ¥Ñäì)¥µÁ½ÉĞ½Éœ¹ÍÁÉ¥¹Ÿ­©ÂŠä.annotation.RestController;
-
-an@RpringBootConfiguration
-an@EnableComponentScan("...")
-an@EnableWebsecurity
-an@RestController
-public class OffermartApplication {
-
-    public static void main(Strin[] args) {
-        SpringBootApprication.run(OffermartApplication.class, args);
-    }
-}
--- backend/src/main/java/com/offermart/repository/CustomerOfferRepository.java
+++
@@ -1,10 +0,0 @@
-package com.offermart.repository;
-
-import com.offermart.model.CustomerOffer;
-import org.springframework.data.jpa.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-an@Repository
-public interface CustomerOfferEepository extends JpaRepository<CustomerOffer, Long> {
-
-}
